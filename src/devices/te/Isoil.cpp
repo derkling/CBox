0b22b907ae75cd2948c425a18ff295cb0eff5ad9
Patrick Bellasi <derkling@gmail.com>
2008-09-30 18:37:58 +0000

    Added new TE devices (stub) support
    git-svn-id: file:///media/hdbkp/svn/c3/trunk/cbox@143 c892b811-77a8-453b-ad2f-bbcecf767e94

---
+++ src/devices/te/Isoil.cpp
@@ -0,0 +1,208 @@
+//*********************************************************************
+//*************  Copyright (C) 2006        DARICOM  ********************
+//*********************************************************************
+//**
+//** The copyright to the computer programs here in is the property of
+//** DARICOM The programs may be used and/or copied only with the
+//** written permission from DARICOM or in accordance with the terms and
+//** conditions stipulated in the agreement/contract under which the
+//** programs have been supplied.
+//**
+//*********************************************************************
+//******************** Module information *****************************
+//**
+//** Project:       ProjectName (ProjectCode/Version)
+//** Description:   ModuleDescription
+//**
+//** Filename:      Filename
+//** Owner:         Developer
+//** Creation date:  21/06/2006
+//**
+//*********************************************************************
+//******************** Revision history *******************************
+//** Revision date       Comments                           Responsible
+//** -------- ---------- ---------------------------------- -----------
+//**
+//**
+//*********************************************************************
+
+
+#include "Isoil.ih"
+
+namespace controlbox {
+namespace device {
+
+Isoil::Isoil(std::string const & logName) :
+	DeviceTE(DeviceTE::ISOIL, logName) {
+	LOG4CPP_DEBUG(log, "Isoil(std::string const &)");
+}
+
+Isoil::~Isoil() {
+	LOG4CPP_DEBUG(log, "Destroying the Isoil device");
+}
+
+
+
+inline exitCode
+Isoil::checksum(const char * buf, unsigned len, char chksum[3], bool tx) {
+	unsigned sum = 0;
+	unsigned index = 0;
+
+	for ( ; index<len; index++) {
+		sum += buf[index];
+	}
+
+	// NOTE:  (value % 256) == (value & 0xFF)
+	sprintf(chksum, "%02x",  (sum & 0xFF));
+
+	// On trasmission we need to SWAP the two bytes
+	if (tx) {
+		chksum[0] ^= chksum[1];
+		chksum[1] ^= chksum[0];
+		chksum[0] ^= chksum[1];
+	}
+
+	LOG4CPP_DEBUG(log, "len [%d], sum [%d, 0x%X], checksum [%s]", len, sum, sum, chksum);
+	return OK;
+}
+
+bool
+Isoil::verifyChecksum(const char * buf, unsigned len) {
+	char rxChksum[] = "\0\0\0";
+	char myChksum[] = "\0\0\0";
+	bool match = false;
+
+#ifdef DARICOMDEBUG
+	if ( d_forceDownload ) return true;
+#endif
+
+	memcpy(rxChksum, buf+len-2, 2);
+	checksum(buf, len-2, myChksum, false);
+
+	LOG4CPP_DEBUG(log, "Checksum rx [%s], computed [%s]", rxChksum, myChksum);
+
+	match = strncasecmp(rxChksum, myChksum, 2);
+	if ( match ) {
+		LOG4CPP_WARN(log, "Checksum mismatch on TE data download");
+	}
+
+	return !match;
+
+}
+
+
+exitCode
+Isoil::downloadEvents(t_eventList & eventList) {
+/*
+	unsigned len, index;
+	unsigned short retry;
+	char buf[DEVICE_TE_SAMPI500_RX_BUFFER];
+	Isoil::t_teResps resp;
+	unsigned device;
+	unsigned downloadedRecords = 0;
+	t_event * event;
+	bool readOk = false;
+	EVENT_CODE(evtLable);
+	exitCode result = OK;
+
+	d_tty->detachedMode(false);
+
+	// Sending download command #54
+	LOG4CPP_DEBUG(log, "Send command 54");
+	write(C54_STX);
+
+	do {
+		// Reading next message
+		len = read(buf, DEVICE_TE_SAMPI500_RX_BUFFER);
+		retry = d_retry+1;
+		while ( !len && --retry) {
+			LOG4CPP_DEBUG(log, "Retrying download...");
+			len = read(buf, DEVICE_TE_SAMPI500_RX_BUFFER);
+		};
+		if (!len) {
+			LOG4CPP_WARN(log, "Device not responding: download ABORTED");
+			// If the TE does not respond we wait until the next download time
+			result = TE_NO_NEW_EVENTS;
+			goto download_end;
+		}
+
+		// Verifying message checksum
+		if ( !verifyChecksum(buf, len-1) ) {
+			LOG4CPP_WARN(log, "Checksum error on downloading message [%d]", downloadedRecords+1);
+			//TODO Aborting download to restart from this record
+			result = TE_RESTART_DOWNLOAD;
+			goto download_end;
+		}
+
+		// NOTE: if this is not an ACK we should really come back and FIX THE CODE
+		resp = checkResponce(buf);
+		if ( resp > NAK_NO_NEW_EVENT ) {
+			LOG4CPP_FATAL(log, "Communication error: %s", verboseNak(resp).c_str());
+			result = TE_NAK_RECEIVED;
+			goto download_end;
+		}
+
+		if ( resp == NAK_NO_NEW_EVENT ) {
+			LOG4CPP_DEBUG(log, "No more events from SAMPI500");
+			if (downloadedRecords) {
+				LOG4CPP_DEBUG(log, "Notify events to be uploaded");
+				result = OK;
+				goto download_end;
+			} else {
+				result = TE_NO_NEW_EVENTS;
+				goto download_end;
+			}
+		}
+
+		// Queue event (removing all un-necessary fields)
+		event = new t_event();
+		event->type = getEventType(buf);
+		buf[len-3] = 0; //Removing leading checksum and line terminator....
+
+	//NOTE: we don't remove the leading address, record number and event type because is used by the
+	//	RAW event formatter
+	// 	event->event = std::string(buf+4);
+		event->event = std::string(buf);
+		LOG4CPP_DEBUG(log, "EVT [%c], [%s]",
+				evtLable[event->type],
+				(event->event).c_str());
+
+		eventList.push_back(event);
+		downloadedRecords++;
+
+		// Sending ACK
+		write(C54_ACK);
+
+	} while (true);
+
+	LOG4CPP_INFO(log, "%d new events downloaded", downloadedRecords);
+
+download_end:
+
+	d_tty->detachedMode(true);
+	return result;
+*/
+	return OK;
+}
+
+
+exitCode
+Isoil::formatEvent(t_event const & p_event, comsys::Command & cmd) {
+	std::ostringstream sbuf("");
+	const char * evt;
+
+	evt = p_event.event.c_str();
+
+	sbuf << std::uppercase << std::setw(2) << std::setfill('0') << std::hex << (unsigned)0x14;
+	cmd.setParam( "dist_evtType", sbuf.str());
+	cmd.setParam("dist_evtData", evt+1);
+	LOG4CPP_DEBUG(log, "RAW SampiTE_550, DIST-CODE [%s], DATA [%s]",
+				sbuf.str().c_str(),
+				evt+4);
+
+	return OK;
+
+}
+
+}// namespace device
+}// namespace controlbox
---
+++ src/devices/te/Somefi.h
@@ -0,0 +1,116 @@
+//*********************************************************************
+//*************  Copyright (C) 2006        DARICOM  ********************
+//*********************************************************************
+//**
+//** The copyright to the computer programs here in is the property of
+//** DARICOM The programs may be used and/or copied only with the
+//** written permission from DARICOM or in accordance with the terms and
+//** conditions stipulated in the agreement/contract under which the
+//** programs have been supplied.
+//**
+//*********************************************************************
+//******************** Module information *****************************
+//**
+//** Project:       ProjectName (ProjectCode/Version)
+//** Description:   ModuleDescription
+//**
+//** Filename:      Filename
+//** Owner:         Developer
+//** Creation date:  21/06/2006
+//**
+//*********************************************************************
+//******************** Revision history *******************************
+//** Revision date       Comments                           Responsible
+//** -------- ---------- ---------------------------------- -----------
+//**
+//**
+//*********************************************************************
+
+
+#ifndef _SOMEFI_H
+#define _SOMEFI_H
+
+#include <controlbox/base/Utility.h>
+#include <controlbox/devices/te/DeviceTE.h>
+
+namespace controlbox {
+namespace device{
+
+/// A Somefi is a DeviceTE for SAMPI550's "Testate Elettroniche".
+/// This class provide access to SAMPI550's TE devices connected to
+/// a TTY (serial) port. The class provide initialization
+/// code and methods to tear up a TE connection and provide support
+/// for TE model's specific features: package format handling,
+/// checksum computation, etc. <br>
+/// Supported models are:
+/// <br>
+/// <h5>Configuration params availables for this class:</h5>
+/// <ul>
+///	<li>
+///		<b>paramname</b> - <i>Default: SAMPITE550_DEFAULT_PARAMNAME</i><br>
+///		Parameter description
+///	</li>
+/// </ul>
+/// @see DeviceTE
+class Somefi : public DeviceTE  {
+
+  public:
+
+	/// Create a new Sampi TE device initially disabled.
+	/// In order to enable it you need to attach a Dispatcher.
+	/// @param logName the log category, this name is prepended by the
+	///		class namespace "daricom.comlibs."
+	Somefi(std::string const & logName = "Somefi");
+
+	/// Class destructor.
+	~Somefi();
+
+  protected:
+
+
+	/// Download events from the TE.
+	/// This method should be implemented by sub-classes to download
+	/// all new events from the TE device and it's expected to store them
+	/// into the provided  list.
+	exitCode downloadEvents(t_eventList & eventList);
+
+	/// Format an event.
+	/// This method should parse the event string and fill some of the command
+	/// parameters; this method is expected to return at least those parameters:
+	/// <ul>
+	///	<li>timestamp</li>
+	///	<li>dist_event</li>
+	///	<li>dist_msgType</li>
+	/// </ul>
+	exitCode formatEvent(t_event const & event, comsys::Command & cmd);
+
+	/// Return the command type associated to a generic TE event.
+	/// This method should be implemented by sub-classes simply returning
+	/// the command type corresponding to the specific device.
+	inline DeviceTE::t_cmdType cmdTypeGenericCode() {
+		return DeviceTE::SEND_GENERIC_SAMPI550;
+	};
+
+	/// Compute a buffer's checksum.
+	/// The checksum could be differently computed if we send
+	/// a message to the TE or if we receive it from the TE.
+	/// @param buf the buffer to witch the checksum should be computed
+	/// @param len the size of the buffer
+	/// @param chksum the computed checksum returned
+	/// @param tx if true compute checksum for a message being sent
+	///	to the TE, by default it compute checksum of a received message.
+	/// @return OK on checksum computation success.
+	inline exitCode checksum(const char * buf, unsigned len, char chksum[3], bool tx = false);
+
+	/// Verify the checksum of the passed message.
+	/// @param msg the message to verify
+	/// @param len the lenght of the passed message
+	/// @return true if the received message checksum is valid
+	bool verifyChecksum(const char * msg, unsigned len);
+
+};
+
+}// namespace device
+}// namespace controlbox
+#endif
+
