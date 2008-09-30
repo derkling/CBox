8f637384647fc6af197dfc4786d96df9f8e05e4e
Patrick Bellasi <derkling@gmail.com>
2008-09-30 18:19:07 +0000

    Added test for base64 library Improved digital sensors test using both DS and ATGPS
    git-svn-id: file:///media/hdbkp/svn/c3/trunk/cbox@137 c892b811-77a8-453b-ad2f-bbcecf767e94

--- src/cboxtest.cpp
+++ src/cboxtest.cpp
@@ -76,11 +76,12 @@ using namespace controlbox;
 
 void print_usage(char * progname);
 int test_comlibs(log4cpp::Category & logger);
+int test_utils(log4cpp::Category & logger);
 int test_devicedb(log4cpp::Category & logger);
 int test_command(log4cpp::Category & logger);
 int test_wsproxy(log4cpp::Category & logger);
 // int test_nmeaparser(log4cpp::Category & logger);
-int test_devicegprs(log4cpp::Category & logger, int sleeptime);
+int test_devicegprs(log4cpp::Category & logger);
 int test_deviceas(log4cpp::Category & logger);
 int test_devicegpio(log4cpp::Category & logger);
 int test_deviceds(log4cpp::Category & logger);
@@ -117,16 +118,18 @@ int main (int argc, char *argv[]) {
 			{"atgpstest", no_argument, 0, 'g'},
 			{"tetest", no_argument, 0, 'i'},
 			{"attest", no_argument, 0, 't'},
+			{"utilstest", no_argument, 0, 'u'},
 			{"nocolors", no_argument, 0, 'y'},
 			{0, 0, 0, 0}
 		};
-	static char * optstring = "abC:c:dehgilnors:twy";
+	static const char * optstring = "abC:c:dehgilnors:tuwy";
 	int c;
 	bool silent = false;
 
 	// TESTS to do
 	bool printHelp = true;
 	bool testComlibs = false;
+	bool testUtils = false;
 	bool testDeviceDB = false;
 	bool testDaricomCommand = false;
 	bool testWSProxyCommandHandler = false;
@@ -140,11 +143,15 @@ int main (int argc, char *argv[]) {
 	bool testDeviceTE = false;
 	bool testATinterface = false;
 
+// log4cpp::Category & pupazza = log4cpp::Category::getInstance("cboxtest");
+// test_utils(pupazza);
+// return 0;
+
 	// Logger configuration
 	std::string confFilename = "/etc/cbox/cbox.conf";
 
 #ifdef CONTROLBOX_DEBUG
-	// Forcinf unbuffered stdout
+	// Forcing unbuffered stdout
 	setlinebuf(stdout);
 #endif
 
@@ -238,6 +245,10 @@ int main (int argc, char *argv[]) {
 			case 'r':
 				silent = true;
 				break;
+			case 'u':
+				testUtils = true;
+				printHelp = false;
+				break;
 			case 'h':
 				print_usage(argv[0]);
 				return EXIT_SUCCESS;
@@ -295,11 +306,18 @@ int main (int argc, char *argv[]) {
 		logger.setPriority(log4cpp::Priority::INFO);
 	}
 
+	logger.debug("Dumping memory statistics (/tmp/memstats)");
+	system("cat /proc/meminfo > /tmp/memstats");
+
 	logger.debug("Runnig tests...");
 
 	if (testComlibs) {
 		logger.debug("----------- Testing comlibs ---");
 		test_comlibs(logger);
+	}
+	if (testUtils) {
+		logger.debug("----------- Testing Utilities ---");
+		test_utils(logger);
 	}
 	if (testDeviceDB) {
 		logger.debug("----------- Testing DeviceDB ---");
@@ -315,7 +333,7 @@ int main (int argc, char *argv[]) {
 	}
 	if (testDeviceGPRS) {
 		logger.debug("----------- Testing DeviceGPRS ---");
-		test_devicegprs(logger, sleeptime);
+		test_devicegprs(logger);
 	}
 	if (testDeviceAS) {
 		logger.debug("----------- Testing DeviceAS ---");
@@ -342,8 +360,12 @@ int main (int argc, char *argv[]) {
 		test_atinterface(logger);
 	}
 
+	logger.debug("Upadting memory statistics (/tmp/memstats)");
+	system("cat /proc/meminfo >> /tmp/memstats");
+
 	logger.debug("----------- Tests Completed ---\n");
 	log4cpp::Category::shutdown();
+
 
 	return EXIT_SUCCESS;
 
@@ -363,6 +385,7 @@ void print_usage(char * progname) {
 	cout << "\t-c, --cycles <count>       Test cycles number" << endl;
 	cout << "\t-y, --nocolors             Disable colors on output" << endl;
 	cout << "\t-l, --comlibstest          Do a Test on comlibs" << endl;
+	cout << "\t-u, --utilstest            Do a Test on Utilities" << endl;
 	cout << "\t-e, --devdbtest            Do a Test on DeviceDB" << endl;
 	cout << "\t-d, --commandtest          Do a Test on DaricomCommand" << endl;
 	cout << "\t-w, --wsproxytest          Do a Test on WSProxyCommandHandler" << endl;
@@ -383,10 +406,24 @@ void print_usage(char * progname) {
 int test_comlibs(log4cpp::Category & logger) {
 
 	controlbox::device::FileWriterCommandHandler * fw = 0;
-	controlbox::comsys::EventDispatcher * ed = 0;
 	controlbox::comsys::CommandDispatcher * cd = 0;
 	controlbox::device::PollEventGenerator * peg = 0;
 	controlbox::comsys::Command * command = 0;
+
+	unsigned i,j;
+	char lable[] = "Param100\0";
+
+#define MEM_CONSUMING_STRING	""							\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"	\
+"VeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongStringVeryLongString"
 
 
 	logger.info("Initializing a FileWriterCommandHandler... ");
@@ -400,7 +437,7 @@ int test_comlibs(log4cpp::Category & logger) {
 	command->setParam( "FloatParam",  1.23 );
 	logger.info("DONE!");
 
-	logger.info("Initializing an CommandDispatcher... ");
+	logger.info("Initializing a CommandDispatcher... ");
 	cd = new controlbox::comsys::CommandDispatcher(fw, false);
 	logger.info("DONE!");
 
@@ -410,7 +447,7 @@ int test_comlibs(log4cpp::Category & logger) {
 	peg = new controlbox::device::PollEventGenerator(2000, cd);
 	logger.info("DONE!");
 
-
+#if 0
 	sleep(5); 		// print 2 messages
 	peg->disable();
 	sleep(5); 		// lose 3 messages
@@ -424,15 +461,47 @@ int test_comlibs(log4cpp::Category & logger) {
 	sleep(6); 		// queue 3 messages
 	cd->resume(true);	// => flush 3 messages
 	sleep(2); 		// print 1 messages
-
+#endif
 
 	logger.info("Shutting down the PollEventGenerator... ");
 	peg->disable();
 	delete peg;
 	logger.info("DONE!");
 
-	logger.info("Shutting down the EventDispatcher... ");
-	delete ed;
+	logger.info("Shutting down the CommandDispatcher... ");
+	delete cd;
+	logger.info("DONE!");
+
+
+
+	logger.info("Testing Command MEMORY handling... ");
+
+	logger.info("Initializing a CommandDispatcher... ");
+	cd = new controlbox::comsys::CommandDispatcher(fw, false);
+	logger.info("DONE!");
+
+	do {
+		command = controlbox::comsys::Command::getCommand(1, Device::EG_POLLER, "MC1", "MemoryCheck");
+
+		for (i=0; i<=(cycles/10); i++) {
+// 			logger.info("Loading long string (%d bytes)...", strlen(MEM_CONSUMING_STRING));
+			command->setParam( "LongStringValue", MEM_CONSUMING_STRING, false);
+// 			logger.info("Loading doubles (100)...");
+			for ( j=0; j<100; j++) {
+				sprintf(lable, "Param%03d", j);
+				command->setParam( lable, (double)j, false );
+			}
+		}
+		logger.warn("DISPATCH -#%03d...", cycles);
+		cd->dispatch(command);
+
+		usleep(sleeptime);
+
+	} while(--cycles);
+
+
+	logger.info("Initializing a CommandDispatcher... ");
+	cd = new controlbox::comsys::CommandDispatcher(fw, false);
 	logger.info("DONE!");
 
 	logger.info("Shutting down the FileWriterCommandHandler... ");
@@ -441,6 +510,199 @@ int test_comlibs(log4cpp::Category & logger) {
 
 	return 0;
 
+}
+
+/// Utilities TEST case
+int test_utils(log4cpp::Category & logger) {
+	typedef struct {
+		u8 c[512];
+		unsigned l;
+	} buffs_t;
+	buffs_t in_buf[] = {
+		{
+		{}, 0 },
+		{
+		{'A'}, 1 },
+		{
+		{'A', 'B'}, 2 },
+		{
+		{'A', 'B', 'C'}, 3 },
+		{
+		{'A', 'B', 'C', 'D'}, 4 },
+		{
+		{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A,
+		0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+		0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20,
+		0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B,
+		0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
+		0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41,
+		0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C,
+		0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+		0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x61, 0x62,
+		0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D,
+		0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78,
+		0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F, 0x80, 0x81, 0x82, 0x83,
+		0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E,
+		0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99,
+		0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4,
+		0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+		0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA,
+		0xBB, 0xBC, 0xBD, 0xBE, 0xBF, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5,
+		0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0,
+		0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB,
+		0xDC, 0xDD, 0xDE, 0xDF, 0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6,
+		0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF, 0xF0, 0xF1,
+		0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC,
+		0xFD, 0xFE, 0xFF,}, 256 },
+		{ // Risposta SAMPI 550 comando 28
+		{	0x01,						// Tipo evento (01: accensione, 02:spegnimento, 06 Erogazione, 05 Carico)
+			0x01, 0x00,					// Progressivo di questo tipo di evento (in questo caso accensione) Little Endian
+			0x0a, 0x11, 0x28, 0x0b, 0x06, 0xd7, 0x07,	// (Timestamp 10:11:28 11/06/2007)
+			0x01,						// Riservato
+			0x0b, 0xf4,					// Checksum riservato
+			//Dati Evento - Tipo 01: accensione (165 bytes)
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 1 (double)
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 2
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 3
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 4
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 5
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 6
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 7
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 8
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 9
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 10
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 11
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore n. 12
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore Trafilamento n. 1
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore Trafilamento n. 2
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore Trafilamento n. 3
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Totalizzatore Trafilamento n. 4
+			0x00,						// unità di misura usata (0: litri 0: kg)
+			0x47, 0x50, 0x4C,				// Sigla prodotto 1 ('G','P','L');
+			0x20, 0x20, 0x20,				// Sigla prodotto 2 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 3 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 4 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 5 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 6 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 7 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 8 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 9 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 10 (' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 11(' ',' ',' ');
+			0x20, 0x20, 0x20,				// Sigla prodotto 12 (' ',' ',' ');
+			// Padding del buffer /filling up 205 bytes)
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			// Message trailer
+			0x34, 0x36,				// Checksum del messaggio (64)
+			0x0d,					// Fine frame
+			}, 221},
+		{
+		{0x02, 0x00, 0x0A, 0x1C, 0x01, 0x01, 0x00, 0x5D, 0x06, 0x22, 0x00,
+		0x0B, 0x3B, 0x33, 0x19, 0x09, 0xFF, 0x07, 0x01, 0xFF, 0xFF, 0x01,
+		0x22, 0x00, 0x00, 0x00, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x0B, 0x3B, 0x1E, 0x19, 0x09, 0xFF, 0x07, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0xFF, 0x03, 0x00, 0x00,
+		0x33, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x34, 0xFF, 0x11,
+		0x41, 0x00, 0x00, 0x00, 0x00, 0x68, 0xFF, 0x11, 0x41, 0x24, 0x04,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x5A, 0x11, 0x41, 0x00,
+		0x00, 0x00, 0x00, 0x04, 0x4A, 0x11, 0x41, 0x7C, 0x02, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x04, 0x41, 0x00, 0x00, 0x00,
+		0x00, 0x18, 0xFF, 0x04, 0x41, 0x00, 0x60, 0xFF, 0x44, 0x00, 0x00,
+		0xFF, 0x3F, 0x62, 0xFF, 0x3C, 0x3B, 0x00, 0x00, 0x16, 0x44, 0x5C,
+		0xFF, 0xFF, 0x41, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x00, 0x00, 0x00,
+		0x00, 0x03, 0x09, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x10,
+		0x28, 0x45, 0x55, 0x52, 0x29, 0x20, 0x47, 0x50, 0x4C, 0x20, 0x20,
+		0x20, 0x20, 0x20, 0x4D, 0x45, 0x54, 0x45, 0x52, 0x20, 0x31, 0x20,
+		0x20, 0x41, 0x44, 0x52, 0x43, 0x4F, 0x44, 0x45, 0x30, 0x31, 0x00,
+		0x10, 0xFF, 0xFF, 0x57, 0x00, 0xFF, 0x30, 0x57, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x53, 0x25,
+		0x35, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x57, 0x00, 0x00, 0x10,
+		0x60, 0x03, 0x78, 0x0F, 0x24, 0x00, 0x35, 0x39, 0x0D}, 229},
+		{
+		{"Many thanks to Bob Trower for this wonderfoul Base64 transcoder"}, 63}
+	};
+	unsigned tests = sizeof(in_buf)/sizeof(buffs_t);
+	unsigned t = 0;
+	//std::ostringstream hex_buf;
+	char hex_buf[4096];
+	char out_buf[512];
+	char str_buf[4096];
+	unsigned pos = 0;
+	unsigned int i, j;
+	unsigned len;
+	bool makeSpace = false;
+
+
+// write(2, in_buf[7].c, in_buf[7].l);
+// return 0;
+
+//Utils::b64enc("Ciao Mondo\n\r", 12, out_buf, 512);
+//cout << out_buf << endl;
+//return 0;
+
+	while (t<tests) {
+
+		//hex_buf.str("");
+		hex_buf[0] = 0;
+		if ( isprint(in_buf[t].c[0]) ) {
+			for (pos=0, i=0; i<in_buf[t].l && pos<(4096-7); i++) {
+				pos += sprintf(hex_buf+pos, "%c", in_buf[t].c[i]);
+				if ( ! ((i+1)%99) ) {
+					pos += sprintf(hex_buf+pos, "\n\t\t\t\t\t\t\t");
+				}
+			}
+		} else {
+			for (pos=0, i=0; i<in_buf[t].l && pos<(4096-7); i++) {
+				pos += sprintf(hex_buf+pos, "0x%02X ", in_buf[t].c[i]);
+				if ( ! ((i+1)%20) ) {
+					pos += sprintf(hex_buf+pos, "\n\t\t\t\t\t\t\t");
+				}
+			}
+		}
+
+		logger.info("Testing Base64 transcoder on buffer [size: %u]:\n\t\t\t\t\t\t\t%s", in_buf[t].l, hex_buf);
+
+		Utils::b64enc((char*)in_buf[t].c, in_buf[t].l, out_buf, 512);
+		//hex_buf.str("");
+		str_buf[0] = 0;
+		for (pos=0, i=0; out_buf[i] && pos<(4096-7); i++) {
+			//hex_buf << out_buf[i]; j++;
+			pos += sprintf(str_buf+pos, "%c", out_buf[i]);
+			if ( ! ((i+1)%(99)) ) {
+				//hex_buf << "\n\t\t\t\t\t\t\t";
+				pos += sprintf(str_buf+pos, "\n\t\t\t\t\t\t\t");
+			}
+		}
+		logger.info("Encoded buffer:\n\t\t\t\t\t\t\t%s", str_buf);
+
+		in_buf[t].c[0] = 0; len=512;
+		Utils::b64dec(out_buf, (char*)in_buf[t].c, len);
+
+		//hex_buf.str("");
+		hex_buf[0] = 0;
+		for (pos=0, i=0; i<len; i++) {
+			//hex_buf << "0x" << setw(2) << hex << setfill('0') << (unsigned short)in_buf[t].c[i] << ", ";
+			pos += sprintf(hex_buf+pos, "0x%02X, ", in_buf[t].c[i]);
+			if ( ! ((i+1)%20) ) {
+				//hex_buf << "\n\t\t\t\t\t\t\t";
+				pos += sprintf(hex_buf+pos, "\n\t\t\t\t\t\t\t");
+			}
+		}
+		logger.info("Decoded buffer [size: %u]:\n\t\t\t\t\t\t\t%s", len, hex_buf);
+
+		t++;
+	};
+
+	return 0;
 }
 
 
@@ -516,7 +778,7 @@ int test_command(log4cpp::Category & logger) {
 	controlbox::comsys::Command * command = 0;
 
 	logger.info("Initializing a DaricomCommand... ");
-	command = controlbox::comsys::Command::getCommand(1, Device::DEVICE_TE, "SampiTE", "SampiTE");
+	command = controlbox::comsys::Command::getCommand(1, Device::DEVICE_TE, "Sampi500", "Sampi500");
 	command->setParam( "StringParam", "ParamValue" );
 	command->setParam( "IntParam", 2 );
 	command->setParam( "FloatParam",  (float)1.23 );
@@ -730,16 +992,15 @@ int test_nmeaparser(log4cpp::Category & logger) {
 #endif
 
 /// GPRS testing
-int test_devicegprs(log4cpp::Category & logger, int sleeptime) {
+int test_devicegprs(log4cpp::Category & logger) {
 
 	controlbox::device::FileWriterCommandHandler * fw = 0;
 	controlbox::comsys::CommandDispatcher * cd = 0;
 	controlbox::device::DeviceGPRS * devGPRS = 0;
-	controlbox::device::DeviceGPIO * devGPIO = 0;
-	controlbox::device::ATcontrol * at = 0;
-	controlbox::QueryRegistry * qr = 0;
 	unsigned short level;
 
+	// Ensuring there are not running PPPD deamons that lock modems TTY's ports
+	system("killall pppd");
 
 	logger.info("---- Initializing a FileWriterCommandHandler... ");
 	fw = new controlbox::device::FileWriterCommandHandler("./filewriter.log");
@@ -770,7 +1031,6 @@ int test_devicegprs(log4cpp::Category & logger, int sleeptime) {
 		logger.info("---- Connection UP AND RUNNRING");
 	} else {
 		logger.error("---- Connection PROBLEMS");
-		goto gprs_shutdown;
 	}
 
 	sleep(2);
@@ -789,8 +1049,6 @@ int test_devicegprs(log4cpp::Category & logger, int sleeptime) {
 		}
 	}
 
-
-gprs_shutdown:
 
 	logger.info("---- Shutting down the DeviceGPRS... ");
 	devGPRS->disconnect();
@@ -930,10 +1188,7 @@ int test_deviceds(log4cpp::Category & logger) {
 	controlbox::device::FileWriterCommandHandler * fw = 0;
 	controlbox::comsys::CommandDispatcher * cd = 0;
 	controlbox::device::DeviceDigitalSensors * devDS = 0;
-
-// 	controlbox::device::DeviceDigitalSensors::t_dsIdList dsIdList;
-	unsigned cSensors, i, j;
-// 	controlbox::device::DeviceAnalogSensors::t_asIdList::iterator aSensorId;
+	controlbox::device::DeviceATGPS * devATGPS = 0;
 	std::ostringstream readedValues;
 
 	logger.info("01 - Initializing a FileWriterCommandHandler... ");
@@ -950,22 +1205,31 @@ int test_deviceds(log4cpp::Category & logger) {
 	devDS->enable();
 	logger.info("DONE!");
 
-	logger.info("04 - Starting Digital Sensors test... ");
+	logger.info("04 - Initializing an ATGPS for Testing... ");
+	devATGPS = controlbox::device::DeviceATGPS::getInstance();
+	devATGPS->setDispatcher(cd);
+	devATGPS->enable();
+	logger.info("DONE!");
+
+	logger.info("05 - Starting Digital Sensors test... ");
 
 
 	sleep(sleeptime);
 
 
+	logger.info("06 - Shutting down the DeviceATGPS... ");
+	delete devATGPS;
+	logger.info("DONE!");
 
-	logger.info("05 - Shutting down the DeviceDS... ");
+	logger.info("07 - Shutting down the DeviceDS... ");
 	delete devDS;
 	logger.info("DONE!");
 
-	logger.info("06 - Shutting down the CommandDispatcher... ");
+	logger.info("08 - Shutting down the CommandDispatcher... ");
 	delete cd;
 	logger.info("DONE!");
 
-	logger.info("07 - Shutting down the FileWriterCommandHandler... ");
+	logger.info("09 - Shutting down the FileWriterCommandHandler... ");
 	delete fw;
 	logger.info("DONE!");
 
