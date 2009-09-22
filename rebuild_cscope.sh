f664bd0bf0154f2220c49d68331da2b0111a10b2
Patrick Bellasi <derkling@gmail.com>
2009-09-22 19:11:26 +0200

    Update cscope generator script
    This patch use the project dir as based path for the cscope database.
    This should be more convenient for code developing and compilation.

--- rebuild_cscope.sh
+++ rebuild_cscope.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd src
-find . -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o -iname '*.ih' > cscope.files
+find ./src -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o -iname '*.ih' > cscope.files
 cscope -b -q -k
-cd -
+
